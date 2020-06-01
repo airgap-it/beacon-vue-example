4ac19a0a339d1ceb9997e010a201b7dee39b9f8a
AndreasGassmann <andreas@andreasgassmann.ch>
2020-06-01 10:00:20 +0000

    feat(beacon): add beacon component

--- src/components/Beacon.vue
+++ src/components/Beacon.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
+      For a guide and recipes on how to configure / customize this project,
+      <br />check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
       >.
@@ -98,7 +98,7 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
-export default class HelloWorld extends Vue {
+export default class Beacon extends Vue {
   @Prop() private msg!: string;
 }
 </script>
