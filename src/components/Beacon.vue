76f71d0934d168e2d5a8d7834dfd0aad5cae04d9
AndreasGassmann <andreas@andreasgassmann.ch>
2020-06-02 12:43:40 +0000

    fix(beacon): initialize variable

--- src/components/Beacon.vue
+++ src/components/Beacon.vue
@@ -72,6 +72,7 @@ export default class Beacon extends Vue {
       address: undefined,
       scopes: undefined,
       operationHash: undefined,
+      taquitoOperationHash: undefined,
     };
   }
 
