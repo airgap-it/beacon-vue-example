650ec98c84117bfcdd260222b20af0ee137a6d04
AndreasGassmann <andreas@andreasgassmann.ch>
2020-06-02 11:34:11 +0000

    feat(logo): add beacon logo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <Beacon msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Vue logo" src="./assets/beacon_logo.svg" />
+    <Beacon />
   </div>
 </template>
 
