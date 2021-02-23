c05dacc3aad6f9dc71810adc5c6877120ac6a218
CharlesAUGU <augucharles@gmail.com>
2021-02-23 11:11:14 +0100

    modifying testset

--- testset/database.sql
+++ testset/database.sql
@@ -1,6 +1,6 @@
 create database smart_link_ICO;
 
-create table kyc (id_kyc varchar(255), type ENUM('USD','EUR','BTC','ETH','XTZ'), adresse_envoi varchar(255), adresse_reception varchar(255), mail varchar(20), primary key (adresse_envoi));
+create table kyc (id_kyc varchar(255), addr_type ENUM('USD','EUR','BTC','ETH','XTZ'), sender_addr varchar(255), reception_addr varchar(255), mail varchar(20), primary key (sender_addr));
 
-create table blockchain (id int NOT NULL AUTO_INCREMENT, adresse varchar(255) not null references kyc(adresse_envoi), montant float(18,8), cours_euro float(18,8), date_tx datetime, primary key (id));
+create table blockchain (id int NOT NULL AUTO_INCREMENT, sender_addr varchar(255) not null references kyc(sender_addr), amount float(18,8), price_euro float(18,8), date_tx datetime, primary key (id));
 
