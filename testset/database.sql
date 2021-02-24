2e4b2c35e2ee188b5fc27cbe314a4a97411949fd
Evy <anastasia.duchesne@protonmail.com>
2021-02-24 17:15:31 +0100

    database update and add of a readme about the test sets

--- testset/database.sql
+++ testset/database.sql
@@ -9,16 +9,15 @@ create table kyc (
     sender_addr varchar(255), 
     reception_addr varchar(255), 
     mail varchar(255),
-    isSmakSend boolean, 
+    is_smak_sent boolean, 
     primary key (sender_addr)
 );
 
 create table blockchain (
     tx_hash varchar(255) NOT NULL, 
-    sender_addr varchar(255) not null references kyc(sender_addr), 
     amount float(18,8), 
     price_euro float(18,8), 
-    date_tx datetime, 
+    tx_date datetime, 
     primary key (tx_hash)
 );
 
